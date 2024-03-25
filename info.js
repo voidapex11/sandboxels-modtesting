68045bd0f1caa3b3a03c1cda6e077d0b1fef461a
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2024-03-25 10:58:29 +0000

    Update info.js

--- info.js
+++ info.js
@@ -1,6 +1,16 @@
 elements.info = {
     category: "mods",
 };
+
+elements.mayo = {
+    color: "#ffffff",
+    behavior: behaviors.LIQUID,
+    category: "liquids",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+};
+
 behaviors.info.tick = function(pixel) {
     alert('hi')
 };
